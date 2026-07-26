219c4f5e617ae514c005359d0bd68ef8b3317654
AndresCarmiol <105026609+AndresCarmiol@users.noreply.github.com>
2026-07-26 08:45:54 -0600

    Level Revisions And Small Changes

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -6,8 +6,25 @@ func _physics_process(_delta: float) -> void:
 	if ScrGlobal.level_time == 0:
 		_disappear()
 		return
-	if use_key:
-		if ScrGlobal.keys_left == 0: _appear()
-		else: _disappear()
 	else:
 		_appear()
+	if use_key:
+		if ScrGlobal.keys_left == 0:
+			_activate()
+		else:
+			_deactivate()
+
+func _deactivate() -> void:
+	if not $area.monitoring:
+		return
+	$area.set_deferred("monitoring", false)
+	$area.set_deferred("monitorable", false)
+	$sprite.texture.region = Rect2(36.0, 0.0, 9.0, 9.0)
+
+func _activate() -> void:
+	if $area.monitoring:
+		return
+	$area.set_deferred("monitoring", true)
+	$area.set_deferred("monitorable", true)
+	$sprite.texture.region = Rect2(27.0, 0.0, 9.0, 9.0)
+	_create_trail()
