310fbb62ff281861446d1782792ba3e95f167465
AndresCarmiol <105026609+AndresCarmiol@users.noreply.github.com>
2026-08-17 22:03:30 -0600

    Finished Custom Text
    Finished perfectioning Custom Text and now is usable and updates in the editor. Also made some minor corrections to the font and small numbers...

--- globals/ScrGlobal.gd
+++ globals/ScrGlobal.gd
@@ -2,6 +2,75 @@ extends Node
 
 const TRAIL_SCENE: PackedScene = preload("res://objects/trail_effect.tscn")
 
+const LETTER_MAP: Dictionary = {
+	"A": Vector2(0, 0), # Line ID = 0 v v v
+	"B": Vector2(1, 0),
+	"C": Vector2(2, 0),
+	"D": Vector2(3, 0),
+	"E": Vector2(4, 0),
+	"F": Vector2(5, 0),
+	"G": Vector2(6, 0),
+	"H": Vector2(7, 0),
+	"I": Vector2(8, 0),
+	"J": Vector2(9, 0),
+	"K": Vector2(10, 0),
+	"L": Vector2(11, 0),
+	"--------- --": Vector2(0, 0), # Line ID = 1 v v v
+	"M": Vector2(0, 1),
+	"N": Vector2(1, 1),
+	"Ñ": Vector2(2, 1),
+	"O": Vector2(3, 1),
+	"P": Vector2(4, 1),
+	"Q": Vector2(5, 1),
+	"R": Vector2(6, 1),
+	"S": Vector2(7, 1),
+	"T": Vector2(8, 1),
+	"U": Vector2(9, 1),
+	"V": Vector2(10, 1),
+	"W": Vector2(11, 1),
+	"----- ------": Vector2(0, 0), # Line ID = 2 v v v
+	"X": Vector2(0, 2),
+	"Y": Vector2(1, 2),
+	"Z": Vector2(2, 2),
+	"-----------": Vector2(0, 0), # Line ID = 3 v v v
+	"0": Vector2(0, 3),
+	"1": Vector2(1, 3),
+	"2": Vector2(2, 3),
+	"3": Vector2(3, 3),
+	"4": Vector2(4, 3),
+	"5": Vector2(5, 3),
+	"6": Vector2(6, 3),
+	"7": Vector2(7, 3),
+	"8": Vector2(8, 3),
+	"9": Vector2(9, 3),
+	"----------------": Vector2(0, 0), # Line ID = 4 v v v
+	".": Vector2(0, 4),
+	",": Vector2(1, 4),
+	"'": Vector2(2, 4),
+	"ç": Vector2(3, 4),
+	"Ç": Vector2(4, 4),
+	":": Vector2(5, 4),
+	";": Vector2(6, 4),
+	"¡": Vector2(7, 4),
+	"!": Vector2(8, 4),
+	"¿": Vector2(9, 4),
+	"?": Vector2(10, 4),
+	" ": Vector2(11, 4),
+	"----- -----------": Vector2(0, 0), # Line ID = 5 v v v
+	"*": Vector2(0, 5),
+	"[": Vector2(1, 5),
+	"]": Vector2(2, 5),
+	"(": Vector2(3, 5),
+	")": Vector2(4, 5),
+	"{": Vector2(5, 5),
+	"}": Vector2(6, 5),
+	"|": Vector2(7, 5),
+	"/": Vector2(8, 5),
+	"-": Vector2(9, 5),
+	"+": Vector2(10, 5),
+	"=": Vector2(11, 5),
+}
+
 @warning_ignore_start("unused_signal")
 signal undo
 signal time_changed(last_time, new_time)
@@ -25,6 +94,7 @@ var score: int = 0
 var played_intro: bool = false
 
 var select_button_id: int = 0
+
 
 func _reload_level() -> void:
 	get_tree().change_scene_to_packed(current_level)
--- main_menu/custom_text.gd
+++ main_menu/custom_text.gd
@@ -1,91 +1,52 @@
+@tool
 extends Node2D
 
-@export_multiline var text: String = "TEST"
+var text_test = (
+	"TEXT EXAMPLE
+SPACE
+A    . ,    (1,2)   + =  - 'Iç 
+ÇHOLAÇ
+B []  ()  {}
+C
+D  34567890
+E !¡ ¿? A:B A;Ñ
+*F   
+* G    j   k   l"
+)
+
+@export_multiline var text: String = "I WISH I WHERE A BIRD!!!"
 @export var text_texture: Resource = preload("res://textures/textureChunk_3.png")
 @export var text_width: int = 3
 @export var text_height: int = 5
 
-var letter_map: Dictionary = {
-	"A": Vector2(0, 0),
-	"B": Vector2(1, 0),
-	"C": Vector2(2, 0),
-	"D": Vector2(3, 0),
-	"E": Vector2(4, 0),
-	"F": Vector2(5, 0),
-	"G": Vector2(6, 0),
-	"H": Vector2(7, 0),
-	"I": Vector2(8, 0),
-	"J": Vector2(9, 0),
-	"K": Vector2(10, 0),
-	"L": Vector2(11, 0),
-	"M": Vector2(0, 1),
-	"N": Vector2(1, 1),
-	"Ñ": Vector2(2, 1),
-	"O": Vector2(3, 1),
-	"P": Vector2(4, 1),
-	"Q": Vector2(5, 1),
-	"R": Vector2(6, 1),
-	"S": Vector2(7, 1),
-	"T": Vector2(8, 1),
-	"U": Vector2(9, 1),
-	"V": Vector2(10, 1),
-	"W": Vector2(11, 1),
-	"X": Vector2(0, 2),
-	"Y": Vector2(1, 2),
-	"Z": Vector2(2, 2),
-	"-----------": Vector2(0, 0),
-	"0": Vector2(0, 3),
-	"1": Vector2(1, 3),
-	"2": Vector2(2, 3),
-	"3": Vector2(3, 3),
-	"4": Vector2(4, 3),
-	"5": Vector2(5, 3),
-	"6": Vector2(6, 3),
-	"7": Vector2(7, 3),
-	"8": Vector2(8, 3),
-	"9": Vector2(9, 3),
-	"----------------": Vector2(0, 0),
-	".": Vector2(0, 4),
-	",": Vector2(1, 4),
-	"'": Vector2(2, 4),
-	"ç": Vector2(3, 4),
-	"Ç": Vector2(4, 4),
-	":": Vector2(5, 4),
-	";": Vector2(6, 4),
-	"¡": Vector2(7, 4),
-	"!": Vector2(8, 4),
-	"¿": Vector2(9, 4),
-	"?": Vector2(10, 4),
-	" ": Vector2(11, 4),
-}
 
 func _draw() -> void:
-	var loop_offset = 0
-	for symbol in text:
-		var text_space = 0
-		if loop_offset > 0:
-			text_space = 1
-		print(loop_offset)
-		var atlas_pos: Vector2 = match_alphabet(symbol)
-		draw_texture_rect_region(
-			text_texture,
-			Rect2(
-				position.x + loop_offset * text_width + text_space,
-				position.y,
-				text_width,
-				text_height
-			),
-			Rect2(
-				atlas_pos.x,
-				atlas_pos.y,
-				text_width,
-				text_height
+	var loop_offset_y = 0
+	for line in text.split("\n"):
+		var loop_offset_x = 0
+		for symbol in line:
+			var atlas_pos: Vector2 = match_alphabet(symbol)
+			draw_texture_rect_region(
+				text_texture,
+				Rect2(
+					loop_offset_x * text_width + loop_offset_x,
+					loop_offset_y * text_height + loop_offset_y,
+					text_width,
+					text_height
+				),
+				Rect2(
+					atlas_pos.x,
+					atlas_pos.y,
+					text_width,
+					text_height
+				)
 			)
-		)
-		loop_offset += 1
+			loop_offset_x += 1
+		loop_offset_y += 1
+
 
 func match_alphabet(letter) -> Vector2:
-	var letter_pos = letter_map.get(letter)
+	var letter_pos = ScrGlobal.LETTER_MAP.get(letter)
 	if letter_pos == null:
-		letter_pos = letter_map["?"]
+		letter_pos = Vector2(11, 11)
 	return Vector2(letter_pos.x * text_width, letter_pos.y * text_height)
